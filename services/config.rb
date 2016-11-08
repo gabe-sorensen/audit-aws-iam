afa24d4b9a71a995e1cc94bfb8a4efc06e21f6e1
George Moberly <georgemoberly@Georges-MacBook-Air-2.local>
2016-11-08 12:26:38 -0800

    global rename of INSTANCE and STACK variables

--- services/config.rb
+++ services/config.rb
@@ -192,14 +192,14 @@ coreo_uni_util_notify "advise-iam" do
   type 'email'
   allow_empty ${AUDIT_AWS_IAM_ALLOW_EMPTY}
   send_on "${AUDIT_AWS_IAM_SEND_ON}"
-  payload '{"stack name":"INSTANCE::stack_name",
-  "instance name":"INSTANCE::name",
-  "number_of_checks":"STACK::coreo_aws_advisor_iam.advise-iam.number_checks",
-  "number_of_violations":"STACK::coreo_aws_advisor_iam.advise-iam.number_violations",
-  "number_violations_ignored":"STACK::coreo_aws_advisor_iam.advise-iam.number_ignored_violations",
-  "violations": STACK::coreo_aws_advisor_iam.advise-iam.report }'
+  payload '{"stack name":"PLAN::stack_name",
+  "instance name":"PLAN::name",
+  "number_of_checks":"COMPOSITE::coreo_aws_advisor_iam.advise-iam.number_checks",
+  "number_of_violations":"COMPOSITE::coreo_aws_advisor_iam.advise-iam.number_violations",
+  "number_violations_ignored":"COMPOSITE::coreo_aws_advisor_iam.advise-iam.number_ignored_violations",
+  "violations": COMPOSITE::coreo_aws_advisor_iam.advise-iam.report }'
   payload_type "json"
   endpoint ({ 
-              :to => '${AUDIT_AWS_IAM_ALERT_RECIPIENT}', :subject => 'CloudCoreo iam advisor alerts on INSTANCE::stack_name :: INSTANCE::name'
+              :to => '${AUDIT_AWS_IAM_ALERT_RECIPIENT}', :subject => 'CloudCoreo iam advisor alerts on PLAN::stack_name :: PLAN::name'
             })
 end
