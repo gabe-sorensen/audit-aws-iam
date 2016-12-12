f7de6a355685033cd873dcbda0044023054b192d
123prove123 <123prove123@gmail.com>
2016-12-12 14:50:54 +0600

    update version

--- services/config.rb
+++ services/config.rb
@@ -216,7 +216,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-iam" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.1.1"
+                   :version => "1.1.7"
                }       ])
   json_input '{ "composite name":"PLAN::stack_name",
                 "plan name":"PLAN::name",
@@ -225,8 +225,26 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-iam" do
                 "number_violations_ignored":"COMPOSITE::coreo_aws_advisor_iam.advise-iam.number_ignored_violations",
                 "violations": COMPOSITE::coreo_aws_advisor_iam.advise-iam.report}'
   function <<-EOH
+  
+const JSON = json_input;
+const NO_OWNER_EMAIL = "${AUDIT_AWS_IAM_ALERT_RECIPIENT_2}";
+const OWNER_TAG = "${AUDIT_AWS_IAM_OWNER_TAG}";
+const AUDIT_NAME = 'iam';
+const IS_KILL_SCRIPTS_SHOW = false;
+const EC2_LOGIC = ''; // you can choose 'and' or 'or';
+const EXPECTED_TAGS = [];
+
+const VARIABLES = {
+    'NO_OWNER_EMAIL': NO_OWNER_EMAIL,
+    'OWNER_TAG': OWNER_TAG,
+    'AUDIT_NAME': AUDIT_NAME,
+    'IS_KILL_SCRIPTS_SHOW': IS_KILL_SCRIPTS_SHOW,
+    'EC2_LOGIC': EC2_LOGIC,
+    'EXPECTED_TAGS': EXPECTED_TAGS
+};
+
 const CloudCoreoJSRunner = require('cloudcoreo-jsrunner-commons');
-const AuditIAM = new CloudCoreoJSRunner(json_input, false, "${AUDIT_AWS_IAM_ALERT_RECIPIENT_2}", "${AUDIT_AWS_IAM_OWNER_TAG}", 'iam');
+const AuditIAM = new CloudCoreoJSRunner(JSON, VARIABLES);
 const notifiers = AuditIAM.getNotifiers();
 callback(notifiers);
   EOH
