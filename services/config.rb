05715be0d4de25bce446f97214557bf6793654ba
George Moberly <georgemoberly@Georges-MacBook-Air-2.local>
2016-12-15 01:32:49 +0600

    clean up ununsed stuff

--- services/config.rb
+++ services/config.rb
@@ -273,7 +273,7 @@ coreo_uni_util_notify "advise-iam-html-report" do
 end
 
 # coreo_uni_util_notify "advise-iam-rollup" do
-#   action :${AUDIT_AWS_IAM_ROLLUP_REPORT}
+#   action :"AUDIT_AWS_IAM_ROLLUP_REPORT" # CANT UNCOMMENT
 #   type 'email'
 #   allow_empty true
 #   send_on '${AUDIT_AWS_IAM_SEND_ON}'
@@ -289,7 +289,7 @@ end
 #   '
 #   payload_type 'text'
 #   endpoint ({
-#       :to => '${AUDIT_AWS_IAM_ALERT_RECIPIENT_2}', :subject => 'CloudCoreo iam advisor alerts on PLAN::stack_name :: PLAN::name'
+#       :to => 'AUDIT_AWS_IAM_ALERT_RECIPIENT_2', :subject => 'CloudCoreo iam advisor alerts on PLAN::stack_name :: PLAN::name' # CANT UNCOMMENT
 #   })
 # end
 =begin
