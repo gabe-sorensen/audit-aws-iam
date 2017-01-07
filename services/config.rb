ea012b002c112c30c7a3c53412dd24be2d6d9947
Ian Coleman <colemai@tcd.ie>
2017-01-06 17:41:53 -0800

    Update config.rb

--- services/config.rb
+++ services/config.rb
@@ -105,12 +105,12 @@ coreo_aws_advisor_alert "iam-no-mfa" do
   suggested_action "Enable Multi-Factor Authentication for every cloud user."
   level "Critical"
   id_map "modifiers.user_name"
-  objectives ["users", "mfa_devices", "login_profile"]
-  formulas ["", "count", ""]
+  objectives ["users", "login_profile", "mfa_devices"]
+  formulas ["", "", "count"]
   call_modifiers [{}, { :user_name => "users.user_name" }, { :user_name => "users.user_name" }]
-  audit_objects ["", "object.mfa_devices", "object.login_profile"]
-  operators ["", "<", "=="]
-  alert_when ["", 1, nil]
+  audit_objects ["", "object.login_profile", "object.mfa_devices"]
+  operators ["", "==", "<"]
+  alert_when ["", nil, 1]
 end
 
 coreo_aws_advisor_alert "iam-root-no-mfa" do
