9f46fd666ab75d064cb518a26dc6e17e9221da9c
colemai <colemai@tcd.ie>
2017-01-19 15:21:11 -0800

    test4

--- services/config.rb
+++ services/config.rb
@@ -75,7 +75,7 @@ coreo_aws_advisor_alert "iam-unusediamgroup" do
   category "Access"
   suggested_action "Ensure that groups defined within IAM have active users in them. If the groups don't have active users or are not being used, delete the unused IAM group."
   level "Warning"
-  objectives ["groups", "group", "group"]
+  objectives ["groups", "group"]
   call_modifiers [{}, { :group_name => "groups.group_name" }]
   formulas ["", "count"]
   audit_objects ["", "users"]
