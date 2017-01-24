4186c5afa7e3beb269033c40ecae99f46feb69d1
colemai <colemai@tcd.ie>
2017-01-23 17:42:36 -0800

    Changed another id map

--- services/config.rb
+++ services/config.rb
@@ -114,7 +114,7 @@ coreo_aws_advisor_alert "iam-active-key-no-rotation" do
   category "Access"
   suggested_action "If you regularly use the AWS access keys, we recommend that you also regularly rotate or delete them."
   level "Critical"
-  id_map "object.access_key_metadata.access_key_id"
+  id_map "modifiers.user_name"
   objectives ["users", "access_keys", "access_keys"]
   audit_objects ["", "access_key_metadata.status", "access_key_metadata.create_date"]
   call_modifiers [{}, {:user_name => "users.user_name"}, {:user_name => "users.user_name"}]
