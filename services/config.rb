b01bfc58fb16d96b2cea73462ffbd8127f2ddb2c
Michael Pautov <123prove123@gmail.com>
2017-02-23 09:04:36 +0600

    Updated cloudcoreo module

--- services/config.rb
+++ services/config.rb
@@ -443,7 +443,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-iam" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.8.3"
+                   :version => "1.8.6"
                },
                {
                    :name => "js-yaml",
