bb88c19d5f56f5b07a6e7b56e321270a224e73e5
Michael Pautov <michael.pautov@gmail.com>
2017-03-09 13:49:34 +0600

    Changed * to latest

--- services/config.rb
+++ services/config.rb
@@ -474,7 +474,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-iam" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "*"
+                   :version => "latest"
                },
                {
                    :name => "js-yaml",
