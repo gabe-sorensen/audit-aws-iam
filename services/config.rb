ec7c328edcce3b51b20c3900aad7a04fbcc91db1
Michael Pautov <michael.pautov@gmail.com>
2017-03-13 16:34:09 +0600

    Set specific jsrunner 1.9.3

--- services/config.rb
+++ services/config.rb
@@ -522,7 +522,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-iam" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "latest"
+                   :version => "1.9.3"
                },
                {
                    :name => "js-yaml",
