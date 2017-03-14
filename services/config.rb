254f834f83e32b995af2549f48993a77bb2463c4
Michael Pautov <michael.pautov@gmail.com>
2017-03-14 13:29:13 +0600

    Updated CloudCoreo JSrunner version 1.9.4

--- services/config.rb
+++ services/config.rb
@@ -522,7 +522,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-iam" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.9.3"
+                   :version => "1.9.4"
                },
                {
                    :name => "js-yaml",
