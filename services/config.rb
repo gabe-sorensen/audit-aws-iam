820c410acd24ecc7276861e50092b62909fec2e1
colemai <colemai@tcd.ie>
2017-03-21 16:01:55 -0700

    Revert "myriad attempts at fix"
    This reverts commit dd421289d9314c2bdf035225758b175dbfba0dfb.

--- services/config.rb
+++ services/config.rb
@@ -557,8 +557,8 @@ coreo_aws_rule "iam-internal" do
   id_map "object.content.user"
   objectives ["credential_report"]
   audit_objects ["object.content.user"]
-  operators ["!="]
-  raise_when [nil]
+  operators ["=~"]
+  raise_when [//]
 end
 
 coreo_uni_util_variables "iam-planwide" do
@@ -591,7 +591,15 @@ coreo_uni_util_jsrunner "cis-iam" do
   data_type "json"
   provide_composite_access true
   packages([
-  json_input '{"violations": COMPOSITE::coreo_aws_rule_runner.advise-iam.report}'
+               {
+                   :name => "cloudcoreo-jsrunner-commons",
+                   :version => "1.9.2"
+               },
+               ])
+  json_input '{ "composite name":"PLAN::stack_name",
+                "plan name":"PLAN::name",
+                "cloud account name": "PLAN::cloud_account_name",
+                "violations":COMPOSITE::coreo_aws_rule_runner.advise-iam.report}'
   function <<-EOH
   
 function copyPropForNewJsonInput() {
